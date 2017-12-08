535e45cad049323dea6e256fae28a7c6a1ef512d
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-12-08 14:56:17 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+#[macro_use]
+extern crate stdweb;
+
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_json;
+
+#[derive(Serialize)]
+struct Person {
+    name: String,
+}
+
+js_serializable!(Person);
+
+fn say_hello(person: Person) {
+    println!("Hello {}!", person.name);
+}
+
+fn main() {
+    stdweb::initialize();
+
+    let name = String::from("Tomek!");
+    say_hello(Person { name: name });
+
+    stdweb::event_loop();
+}
