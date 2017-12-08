720a857d8a06a7d79eb474d2b2b68eb6b57790d4
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2017-12-08 15:54:02 +0100

    Add counter example

--- Makefile
+++ Makefile
@@ -2,5 +2,6 @@ release:
 	@cargo web build --release
 	@rm -rf dist
 	@mkdir dist
-	@cp target/asmjs-unknown-emscripten/release/wasm-test.js dist/app.js
-	@cp public/index.html dist/index.html
+	@mkdir dist/js
+	@cp target/asmjs-unknown-emscripten/release/wasm-test.js dist/js/app.js
+	@cp static/index.html dist/index.html
--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,60 @@ extern crate stdweb;
 extern crate serde_derive;
 extern crate serde_json;
 
-#[derive(Serialize)]
-struct Person {
-    name: String,
+use stdweb::unstable::TryInto;
+use stdweb::web::{IEventTarget, IElement, IHtmlElement, INode, HtmlElement, Element, document,
+                  window};
+
+use stdweb::web::event::{IEvent, IKeyboardEvent, DoubleClickEvent, ClickEvent, KeypressEvent,
+                         ChangeEvent, BlurEvent, HashChangeEvent};
+
+use stdweb::web::html_element::InputElement;
+
+// #[derive(Serialize)]
+// struct Person {
+//     name: String,
+// }
+
+// Shamelessly stolen from webplatform's TodoMVC example.
+macro_rules! enclose {
+    ( ($( $x:ident ),*) $y:expr ) => {
+        {
+            $(let $x = $x.clone();)*
+            $y
+        }
+    };
 }
 
-js_serializable!(Person);
+// js_serializable!(Person);
 
-fn say_hello(person: Person) {
-    println!("Hello from Rust {}!", person.name);
-}
+// fn say_hello(person: Person) {
+//     println!("Hello from Rust {}!", person.name);
+// }
 
 fn main() {
     stdweb::initialize();
 
-    let name = String::from("Tomek!");
-    say_hello(Person { name: name });
+    let mut counter = 0;
+
+    let root = document().get_element_by_id("root").unwrap();
+
+    // let name = String::from("Tomek!");
+    // say_hello(Person { name: name });
+
+    let button = document().create_element("button");
+    let count_display = document().create_element("span");
+
+    button.append_child(&document().create_text_node("click me"));
+    button.add_event_listener(enclose!((count_display) move |_: ClickEvent| {
+        counter += 1;
+        println!("Button clicked {} times!", counter);
+        count_display.set_text_content(counter.to_string().as_str());
+    }));
+
+    count_display.set_text_content(counter.to_string().as_str());
+
+    root.append_child(&button);
+    root.append_child(&count_display);
 
     stdweb::event_loop();
 }
